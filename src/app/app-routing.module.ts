e53c279335698fed0750c4ce63efbf734020cbbb
PedroPK <pedropk@gmail.com>
2022-05-29 20:01:50 -0300

    Added a Default Route to AppRoutingModule - If a non specified path is sent, redirect to the PhotoList Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,9 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 const routes: Routes = [
-	{
-		path: 'user/flavio',
-		component: PhotoListComponent
-	},
-	{
-		path: 'p/add',
-		component: PhotoFormComponent
-	}
+	{	path: 'user/flavio',	component: PhotoListComponent	},
+	{	path: 'p/add',			component: PhotoFormComponent	},
+	{	path: '**',				component: PhotoListComponent	}
 ];
 
 @NgModule({
