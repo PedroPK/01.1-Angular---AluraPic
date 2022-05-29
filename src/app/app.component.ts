707959fee48594cf62f68e1fd4c06b23709deae2
PedroPK <pedropk@gmail.com>
2022-05-28 22:02:21 -0300

    Consuming Backend API to bring Photos
    Injecting a HttpClient on the Constructor
    
    Using Get() to specify the UTL to be consumed
    
    Invoking Subscribe() to make us Observers and effectivily access the URL
    
    Using an Arrow Function as a Parameter in Subsctibe()
    
    In the Arrow function, assigning the result array into the Photos[] Attribute
    
    Solving a bug of Typing, change the Attribute from Object[] to any[]
     - https://cursos.alura.com.br/forum/topico-imagens-nao-carregam-pois-os-atributos-url-e-description-nao-sao-reconhecidos-202835

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient }	from '@angular/common/http';
+import { Component }	from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,23 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'Alura Pic';
+	photos: any[] = [];
 
 	constructor(httpClient: HttpClient) {
-		console.log(httpClient);
+		httpClient
+			.get<Object[]>('http://localhost:3000/flavio/photos')
+			.subscribe( // Observable is a Lazy object. It will not access the URL unless there is an Observer.
+				photos => {
+					console.log(photos)
+					this.photos = photos
+				}
+			); 
 	}
 
-	photos = [
-		{
-			url: 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Sultan_the_Barbary_Lion.jpg',
-			description: 'Leão da Barbária'
-		}, 
-		{
-			url: 'https://upload.wikimedia.org/wikipedia/commons/c/c6/Okonjima_Lioness.jpg',
-			description: 'Leoa de Okinjima'
-		}, 
-		{
-			url: 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Mountain_lion_Winter.jpg',
-			description: 'Leão da Montanha'
-		}, 
-	];
+	
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule }				from '@angular/core';
+import { BrowserModule }		from '@angular/platform-browser';
+import { HttpClientModule }		from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PhotosModule } from './photos/photos.module';
+import { AppRoutingModule }		from './app-routing.module';
+import { AppComponent }			from './app.component';
+import { PhotosModule }			from './photos/photos.module';
 
 @NgModule({
   declarations: [
