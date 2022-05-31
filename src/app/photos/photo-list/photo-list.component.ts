41dae402d7ce28e72402e2bf47ceb677743600ba
PedroPK <pedropk@gmail.com>
2022-05-30 21:41:33 -0300

    Implemented the OnDestroy interface  - Using it to free up memory and processing time

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit }		from '@angular/core';
+import { Component, OnDestroy, OnInit }		from '@angular/core';
 import { ActivatedRoute }			from '@angular/router';
 import { debounceTime, Subject }	from 'rxjs';
 import { Photo }					from '../photo/photo';
@@ -9,7 +9,7 @@ import { Photo }					from '../photo/photo';
   styleUrls: ['./photo-list.component.css']
 })
 // Now this Component is responsible only to consume Service and Store the Photo array
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy  {
 								// avoid any runtime error if ngOnInit() is not defined correctly
 	photos: Photo[] = [];
 	filter: string	= '';
@@ -27,15 +27,21 @@ export class PhotoListComponent implements OnInit {
 	// Phase On Init occurs after Instacialization and after this component receive the Inbound properties.
 	ngOnInit(): void {
 		/*
-		  Replacing the Service consumption to consume the PhotoListResolver.
-		  Now we are retrieving final data to PhotoList Template, with Filter applyed a priori, 
-		  without the need to load all data and then refresh the Template moments after Filter execute
-		 */
+		Replacing the Service consumption to consume the PhotoListResolver.
+		Now we are retrieving final data to PhotoList Template, with Filter applyed a priori, 
+		without the need to load all data and then refresh the Template moments after Filter execute
+		*/
 		this.photos		=	this.activatedRoute.snapshot.data['photos'];
-
+		
 		this.debouncer
-			.pipe(debounceTime(500))
-			.subscribe(filter => this.filter = filter);
+		.pipe(debounceTime(500))
+					// filter (not this.filter) is coming from the PhotoList Template (ilterByDescription: filter)
+		.subscribe(filter => this.filter = filter);
+	}
+	
+	ngOnDestroy(): void {
+		// Stop the observation and freeing memory and processing time
+		this.debouncer.unsubscribe();
 	}
 
 }
